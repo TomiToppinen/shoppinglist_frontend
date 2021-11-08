8c1cd1fc09a816632589d26846cb605564094e68
Tomi Toppinen <n0toto00@students.oamk.fi>
2021-11-08 23:10:36 +0200

    viedään

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+
+const URL = 'http://localhost/shoppinglist/';
 
 function App() {
+  const [tasks, setTasks] = useState ([]);
+  const [task, setTask] =  useState ('');
+
+  useEffect(() => {
+    axios.get(URL)
+    .then((response)=> {
+      console.log(response.data)
+      setTasks(response.data);
+    }).catch(error => {
+      alert(error);
+    })
+
+  },[])
+
+  function save(e) {
+    e.preventDefault();
+    const json = JSON.stringify({description:task})
+    axios.post(URL + 'add.php',json,{
+      headers: {
+        'Content-Type' : 'application/json'
+      }
+    })
+
+    .then((response) => {
+      setTasks(tasks => [...tasks,response.data]);
+      setTask('');
+    }).catch (error => {
+      alert(error.response.data.error)
+    });
+
+    }
+
+  
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <><div className="container">
+      <h3>Shopping list</h3>
+      <form onSubmit={save}>
+        <label>New item</label>
+        <input value={task} onChange={e => setTask(e.target.value)} />
+        <button>Add</button>
+      </form>
+    </div><div className="container">
+        <ol>
+          {tasks?.map(task => (
+            <li key={task.id}>{task.description}</li>
+          ))}
+        </ol>
+      </div></>
+    
   );
 }
 
